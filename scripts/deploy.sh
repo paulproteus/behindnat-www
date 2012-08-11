055913b1c232624bfd604756b1410708a1cd210c
Asheesh Laroia <asheesh@asheesh.org>
2012-08-11 11:27:20 -0700

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 SCRATCH_SPACE=$(mktemp -d)
 chmod 755 "$SCRATCH_SPACE"
-git archive --format=tar | (cd "$SCRATCH_SPACE" ; tar xf -)
-rsync --delete -avz "$SCRATCH_SPACE"/. paulproteus@rose.makesad.us:/tmp/roflcopter/.
+git archive --format=tar HEAD | (cd "$SCRATCH_SPACE" ; tar xf -)
+rsync --delete -avz "$SCRATCH_SPACE"/. asheeshweb@rose.makesad.us:/home/asheeshweb/web/behindnat.com/.
